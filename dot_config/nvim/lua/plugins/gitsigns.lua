da01cfe2f88635f1160566f84a3fe0fb3ce6e636
qinglong yu <qinglong.yu23@gmail.com>
2024-03-22 17:53:40 +0800

    Add gitsigns config in nvim & Using zoxide instead of z

---
+++ dot_config/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,11 @@
+return {
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			-- add your options that should be passed to the setup() function here
+			current_line_blame = true,
+			numhl = true,
+			word_diff = true,
+		},
+	},
+}
